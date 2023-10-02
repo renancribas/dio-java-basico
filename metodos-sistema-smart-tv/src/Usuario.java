a5caa5f2a4f0bbdd7e0307db3dd9195d6c077b05
Renan C. Ribas <renancidade@gmail.com>
2023-10-02 20:45:02 -0300

    Projeto smart TV

---
+++ metodos-sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,20 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+        smartTv.diminuirVolume();
+
+        System.out.println("Canal Atual: " + smartTv.canal);
+        smartTv.mudarCanal(15);
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+
+    }
+}
