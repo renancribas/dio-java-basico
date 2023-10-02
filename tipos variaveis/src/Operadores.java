4855a0137e58eac1e4cb7382abe3f08572610d3d
Renan C. Ribas <renancidade@gmail.com>
2023-10-02 19:12:46 -0300

    Operadores java

---
+++ tipos variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+  public static void main(String[] args){ 
+    int numero1 = 1;
+    int numero2 = 1;
+
+    if(numero1== 2 & numero2 ++ == 2 )
+    System.out.println("As 2 condições são verdadeiras");
+
+    System.out.println("O numero 1 agora é " + numero1);
+    System.out.println("O numero 2 agora é " + numero2);
+  }
+  
+}
