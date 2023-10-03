a7b0d85f4e42450d1013a0f249e52850114c90ef
Renan C. Ribas <renancidade@gmail.com>
2023-10-03 17:23:04 -0300

    finalizado

--- desafio-sintaxe/ContaBanco/src/ContaTerminal.java
+++ desafio-sintaxe/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite seu nome para identificação: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+        scanner.close();
     }
 }
